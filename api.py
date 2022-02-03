70cc77231e040a401b8fdad2686b795bac184d35
Robert Parks <r1parks.ucsd@gmail.com>
2022-02-03 18:12:27 -0500

    added exceptions for HTTP errors

--- api.py
+++ api.py
@@ -9,12 +9,14 @@ BASE_URL = 'https://wordle.bobparks.info'
 def start_new_game():
     url = urljoin(BASE_URL, 'new/')
     response = requests.get(url)
+    response.raise_for_status()
     return response.json()
 
 
 def get_game_status(game_id):
     url = urljoin(BASE_URL, f'status/{game_id}/')
     response = requests.get(url)
+    response.raise_for_status()
     return response.json()
 
 
@@ -22,4 +24,5 @@ def make_guess(game_id, guess):
     url = urljoin(BASE_URL, 'guess/')
     json_data = {'game_id': game_id, 'guess': guess}
     response = requests.post(url, json=json_data)
+    response.raise_for_status()
     return response.json()
